ba922ca04f86156d620b7c0a3934ff169b8805f9
KevX36 <kevincard2007@gmail.com>
2026-03-23 15:20:13 -0300

    got a bit done will work from home

--- Assets/Systems/DialogueManager.cs
+++ Assets/Systems/DialogueManager.cs
@@ -1,20 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
 
-    private Queue<string> words = new Queue<string>();
-    
-    
+    protected Queue<string> dialogue;
+
+    private void Start()
+    {
+        dialogue = new Queue<string>();
+    }
     public void startDialogue(string name,string[] text)
     {
         Debug.Log("talking");
+        dialogue.Clear();
+
+
+        foreach (string line in text)
+        {
+            dialogue.Enqueue(line);
+        }
+
+
+
+
+
     }
 
+    
 
+    
 
 
 }
