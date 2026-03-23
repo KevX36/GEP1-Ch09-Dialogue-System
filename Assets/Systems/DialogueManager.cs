aa24c0bf33a8b1df823d0e1722772edf0943d7df
KevX36 <kevincard2007@gmail.com>
2026-03-23 14:52:06 -0300

    Initial commit

---
+++ Assets/Systems/DialogueManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    private Queue<string> words = new Queue<string>();
+    
+    
+    public void startDialogue(string name,string[] text)
+    {
+        Debug.Log("talking");
+    }
+
+
+
+
+}
---
+++ Assets/Systems/Interaction/Interactable Types/InteractableDialogue.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class InteractableDIalogue : MonoBehaviour, IInteractable
+{
+    public ServiceHub hub;
+    public string name;
+    public string Dialogue;
+    public void Interact()
+    {
+
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
